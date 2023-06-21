644b1240eed76b6b75aa2eaa0a0380045febcf7d
Julius Ojakaa <ojayallday@gmail.com>
2023-06-21 04:24:27 +0300

    header component, bootstrap and font favorite added

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import React from 'react';
+import '../App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <React.Fragment>
+      <div>
+      <Header/>
+        
+      </div>
+    </React.Fragment>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+function Header(){
+    return(
+        <React.Fragment>
+            <div className='section-1'>
+               <div className="container">
+                   <div className="row">
+                       <div className="col-12">
+                           <div className="title d-flex">
+                               <div className="justify-content-center align-self-center w-100">
+                                   <div className="big-title text-center">Our Clothing, Your Comfort</div>
+                                   <div className="title-desc text-center">
+                                       We understand that you need to look good and feel great. <br/>
+                                       It all starts with what you are wearing.                                
+                                   </div>
+                               </div> 
+                           </div>
+                       </div>
+
+                       <div className="col-12">
+                           <div className="text-center chevron-down">
+                               <a href="#products">
+                                   <i className="fa fa-chevron-down" aria-hidden="true"></i>
+                               </a>
+                           </div>
+
+                       </div>
+
+                   </div>
+               </div>
+
+           </div>
+        </React.Fragment>
+    )
+}
+
+export default Header
